0685af8ee79112de471241c367a91ffd0c9caff6
Alex-Harron <aharron.harron@gmail.com>
2021-02-24 20:36:57 -0500

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, 'mysecret'
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get "/" do
+    erb :welcome
+  end
+
+end
