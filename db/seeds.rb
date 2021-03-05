09dbfec03404759efe42c27e1f18ad575fc83c8f
Alex-Harron <aharron.harron@gmail.com>
2021-03-05 14:15:40 -0500

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require_relative '../config/environment'
 User.create(username: "Big_Balla", email: "BB@gmail.com", password: "1234", img_url:'https://logos-download.com/wp-content/uploads/2016/04/LA_Clippers_logo_logotype_emblem.png')
 User.create(username: "Clips_Fan", email: "Clips@gmail.com", password: "1234", img_url:'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/6450.png')
 User.create(username: "Kawhi's_Hands", email: "BigHands@gmail.com", password: "1234", img_url:'https://i.pinimg.com/originals/52/37/c5/5237c564b9daa40b2ad3b953de84a113.jpg')
-User.create(username: "Doc_Rivers", email: "DocRivers@gmial.com", password: "1234", img_url:'https://i.pinimg.com/originals/d6/51/af/d651af2834ef44dbc2f8c43433c6b4a9.jpg')
+User.create(username: "The_Doc", email: "DocRivers@gmial.com", password: "1234", img_url:'https://i.pinimg.com/originals/d6/51/af/d651af2834ef44dbc2f8c43433c6b4a9.jpg')
 
 Post.create(title: "New year, new coach. This team is ballin!", content: Faker::Lorem.paragraph_by_chars, timestamp: Faker::Time.between(from: DateTime.now - 365, to: DateTime.now), user_id: User.all.sample.id)
 Post.create(title: "Kawhi is killing it this year!!!", content: Faker::Lorem.paragraph_by_chars, timestamp: Faker::Time.between(from: DateTime.now - 365, to: DateTime.now), user_id: User.all.sample.id)
